a5abd3b40b2ad8b60c8c7a636567c52db450474b
Cintia <cintiadiasbilbao@gmail.com>
2025-02-12 10:37:26 +0100

    dia-09 complete

--- dia-10/main.py
+++ dia-10/main.py
@@ -41,6 +41,7 @@ operations = {
 function = operations ["+"]
 function(2,3)
 
+
 num1 = int(input("What is the first number?"))
 num2 = int(input("What is the second number?"))
 for  symbol in operations:
@@ -48,6 +49,12 @@ for  symbol in operations:
 operation_symbol = input("Pick an operation from the line above:")
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1,num2)
+first_answer = calculation_function(num1,num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation:")
+num3 = int(input("What is the next number?"))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1,num2),num3)
+print(f"{first_answer}{operation_symbol}{num3} = {second_answer}")
