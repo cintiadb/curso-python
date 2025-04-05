0d56aa7bbdfb3331cd1ecc9e1950ee7395bdee7d
Cintia <cintiadiasbilbao@gmail.com>
2025-04-05 13:50:51 +0200

    parte7

--- dia-05/handling_json.py
+++ dia-05/handling_json.py
@@ -1,13 +1,14 @@
 import json
 
 def print_persons(filename: str):
-    with open("person.json","r") as my_file:
-        data = my_file.read()
-
+    with open(filename,"r") as my_file:
+        data = json.load(my_file) 
         for person in data:
-            name = person['name']
-            age = person['age']
-            hobbies = person['hobbies']
+            name = person["name"]
+            age = person["age"]
+            hobbies = person["hobbies"]
             hobbies_str = ', '.join(hobbies)
             print(f"{name} {age} years ({hobbies_str})")
 
+
+print_persons(filename="person.json")
